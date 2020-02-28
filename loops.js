9811000e2025df6154ec1babfdd42b1a94a3d18c
Theo Booth <theorlbooth@googlemail.com>
2020-02-28 16:01:42 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,3 +16,15 @@ function whileLoop(number) {
   }
   return "done"
 }
+
+var i = 0;
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function doWhileLoop(int) {
+  do {
+    console.log("I run once regardless.")
+  } while (incrementVariable() < int)
+}
